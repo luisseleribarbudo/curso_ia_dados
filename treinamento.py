0b95dc8f7850a2aa37ff730b0d179bcbb5164b52
luisseleribarbudo <luis.seleri.barbudo@gmail.com>
2026-07-03 20:42:21 -0300

    Curso analise de dados com IA, trabalhando com pandas, numpy, seaborn

---
+++ treinamento.py
@@ -0,0 +1,9 @@
+print("Boa noite!")
+
+idade = int(input("Me fale sua idade"))
+idade2 = str(input("Me fale sua idade"))
+idade3 = float(input("Me fale sua idade"))
+
+print(type(idade))
+print(type(idade2))
+print(type(idade3))
